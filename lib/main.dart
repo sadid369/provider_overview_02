b430fffc8c93406b1b344b4076aff6772cb05a19
Sadid369 <sadid.jones@gmail.com>
2022-04-21 19:48:53 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_overview_02/models/dog.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Provider<Dog>(
+      create: (context) => Dog(name: "Sun", breed: "Bulldog", age: 3),
+      child: MaterialApp(
+        title: 'Provider 02',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Provider 02"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              " - name: ${Provider.of<Dog>(context).name}",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            BreedAndAge(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BreedAndAge extends StatelessWidget {
+  const BreedAndAge({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "- breed:${Provider.of<Dog>(context).breed}",
+          style: TextStyle(fontSize: 20),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Age(),
+      ],
+    );
+  }
+}
+
+class Age extends StatelessWidget {
+  const Age({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "- age: ${Provider.of<Dog>(context).age}",
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Provider.of<Dog>(context, listen: false).grow();
+            },
+            child: Text(
+              "Grow",
+              style: TextStyle(fontSize: 20),
+            ))
+      ],
+    );
+  }
+}
